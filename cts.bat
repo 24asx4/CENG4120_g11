75a4d7f56567f18764f66420683d0f3c1ed85d40
24asx4 <24asasasas@gmail.com>
2024-04-27 01:37:26 +0800

    easy for testing

---
+++ cts.bat
@@ -0,0 +1,8 @@
+call g++ -o cts.exe cts.cpp
+call cts --input test0.in --output test0.out 
+call cts --input test1.in --output test1.out
+call cts --input test2.in --output test2.out
+call cts --input test3.in --output test3.out
+call cts --input test4.in --output test4.out
+call cts --input test5.in --output test5.out
+pause 
---
+++ eval.bat
@@ -0,0 +1,7 @@
+cmd /c start python eval.py --input test0.in --output test0.out --plot true
+cmd /c start python eval.py --input test1.in --output test1.out --plot true
+cmd /c start python eval.py --input test2.in --output test2.out --plot true
+cmd /c start python eval.py --input test3.in --output test3.out --plot true
+cmd /c start python eval.py --input test4.in --output test4.out --plot true
+cmd /c start python eval.py --input test5.in --output test5.out --plot true
+pause
