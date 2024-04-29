8ad542908e0d30dd507497977514739c7101eaf4
24asx4 <24asasasas@gmail.com>
2024-04-29 20:09:32 +0800

    typo correction

--- all.bat
+++ all.bat
@@ -11,8 +11,8 @@ call cts --input test4.in --output test4.out
 cmd /c start python eval.py --input test4.in --output test4.out --plot true
 call cts --input test5.in --output test5.out
 cmd /c start python eval.py --input test5.in --output test5.out --plot true
-call cts --input test5.in --output test6.out
+call cts --input test6.in --output test6.out
 cmd /c start python eval.py --input test6.in --output test6.out --plot true
-call cts --input test5.in --output test7.out
+call cts --input test7.in --output test7.out
 cmd /c start python eval.py --input test7.in --output test7.out --plot true
 pause
