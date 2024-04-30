bddb2e40c786c7e4b692763bd3dfbd8489868235
24asx4 <24asasasas@gmail.com>
2024-05-01 01:31:27 +0800

    futher improve v4

--- cts.cpp
+++ cts.cpp
@@ -224,7 +224,7 @@ void reset_point(vector <int> &cur_x, vector <int> &cur_y, vector <vector<int> >
                 to_index_by_pin=j;
             }
         }
-        if (min_by_pin<=manhattan_distance(pin_shortest_point.at(j).at(0),pin_shortest_point.at(j).at(1),pin_shortest_point.at(j).at(2),pin_shortest_point.at(j).at(3))){
+        if (min_by_pin<manhattan_distance(pin_shortest_point.at(j).at(0),pin_shortest_point.at(j).at(1),pin_shortest_point.at(j).at(2),pin_shortest_point.at(j).at(3))){
             pin_shortest_point.at(j).at(0)=from_x_by_pin;
             pin_shortest_point.at(j).at(1)=from_y_by_pin;
             pin_shortest_point.at(j).at(2)=to_x_by_pin;
@@ -540,7 +540,7 @@ int main(int argc, char * argv[]){
                     min_by_pin_index=j;
                 }
             }
-            if (min>min_by_pin){
+            if (min>=min_by_pin){
                 from_x=pin_shortest_point.at(min_by_pin_index).at(0);
                 from_y=pin_shortest_point.at(min_by_pin_index).at(1);
                 to_x=pin_shortest_point.at(min_by_pin_index).at(2);
